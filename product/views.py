e75aefd8a47653439ab2f2b35d85b74a55cc1ee4
Mikhail <110410802+Lunyakin@users.noreply.github.com>
2023-06-28 21:54:07 +0300

    New commit

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
@@ -32,20 +33,22 @@ class BrandViewSet(viewsets.ViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     """A simple Viewset for viewing all products"""
 
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().isactive()
+
     lookup_field = 'slug'
 
     def retrieve(self, request, slug=None):
-        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
-        return Response(serializer.data)
+        serializer = ProductSerializer(self.queryset.filter(slug=slug).select_related('category', 'brand'), many=True)
+        data = Response(serializer.data)
+        return data
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
 
-    @action(methods=['get'], detail=False, url_path=r'category/(?P<category>\w+)/all', url_name='all')
-    def list_product_by_category(self, request, category=None):
+    @action(methods=['get'], detail=False, url_path=r"category/(?P<slug>[\w-]+)", url_name='all')
+    def list_product_by_category_slug(self, request, slug=None):
         """An endpoint to return products by category"""
-        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
+        serializer = ProductSerializer(self.queryset.filter(category__slug=slug), many=True)
         return Response(serializer.data)
